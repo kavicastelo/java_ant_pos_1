691ac9681926c6ab4341e19754afc2ffddfd1d6f
KAVI <kavindu123450@gmail.com>
2023-09-03 17:26:50 +0530

    java ant project initialize and add few files from java pos 2 project

---
+++ src/pos_java_ant_1/POS_JAVA_ant_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pos_java_ant_1;
+
+/**
+ *
+ * @author KAVI
+ */
+public class POS_JAVA_ant_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
